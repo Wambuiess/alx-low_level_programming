08b4dda9c2dd8c01696010f17d4d71e6a97d41f7
Wambui Ess <wambuigitau27@gmail.com>
2022-06-08 15:04:33 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - function
- *
- * Return:return 0
+ * *main prints - prints _putcha, followed by new line
+ * Return: Always 0
  */
-int main (void)
-{
-	char school[10] = "_putchar";
+int main(void)
+{ 
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	int i;
-	
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(school[i];
-	}
-	_putchar(10)
 	return (0);
 }
