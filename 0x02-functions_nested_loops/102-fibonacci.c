eb026a8acef789ba0d5f3dd25f19f0742237ab0d
Wambui Ess <wambuigitau27@gmail.com>
2022-06-08 19:53:05 -0700

    101

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 
 	k = 2;
 
-	for (i = 1; i <=50; ++i)
+	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
 		{
 			printf("%ld, ", j);
 		} else
-                {
+		{
 			printf("%ld\n", j);
 		}
 		next = j + k;
